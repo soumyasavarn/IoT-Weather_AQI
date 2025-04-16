ec9bd3d1aff06113b0665138b9141c45d23451b7
Soumya Savarn <soumyasavarn2@gmail.com>
2025-04-16 12:28:51 +0530

    SERVER_IP updated

--- mock_simulation.py
+++ mock_simulation.py
@@ -12,7 +12,7 @@ import random
 # Configuration
 SSID = "SOUMYA 6843"
 PASSWORD = "4e%8R843"
-SERVER_IP = "192.168.166.11"
+SERVER_IP = "10.150.36.127"
 SERVER_PORT = 5000
 
 pollutants = ['avg_aqi', 'co', 'no', 'no2', 'o3', 'so2', 'pm2_5', 'pm10', 'nh3']
